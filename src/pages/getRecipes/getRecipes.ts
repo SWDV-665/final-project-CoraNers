aae1a068cc6c02c48891c7d1f4dfc5daa4c6edc0
CoraNers <cners1@live.maryville.edu>
2020-11-21 19:22:47 -0500

    WIP - don't test!
    Added tesseract - OCR isn't available w/ ionic v3.  Added some code to try to get this working....

--- src/pages/getRecipes/getRecipes.ts
+++ src/pages/getRecipes/getRecipes.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, ToastController } from 'ionic-angular';
 import { DataServiceProvider } from '../../providers/data-service/data-service';
-import { FavoritesServiceProvider } from '../../providers/favorites-service/favorites-service';
+import { Camera, CameraOptions, PictureSourceType } from '@ionic-native/camera';
+import { ActionSheetController } from 'ionic-angular';
+import * as Tesseract from 'tesseract.js';
 
 @Component({
   selector: 'page-getRecipes',
@@ -9,8 +11,62 @@ import { FavoritesServiceProvider } from '../../providers/favorites-service/favo
 })
 export class GetRecipesPage {
 
-  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public dataService: DataServiceProvider) {
+  selectedImage: string;
+  imageText: string;
 
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public dataService: DataServiceProvider,
+    private camera: Camera, private actionSheetCtrl: ActionSheetController ) {
+
+  }
+
+  selectSource() {
+    let actionSheet = this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Use Library',
+          handler: () => {
+            this.loadPhoto(this.camera.PictureSourceType.PHOTOLIBRARY);
+          }
+        }, {
+          text: 'Capture Image',
+          handler: () => {
+            this.loadPhoto(this.camera.PictureSourceType.CAMERA)
+          }
+        }, {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  loadPhoto(sourceType: PictureSourceType) {
+    
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: sourceType,
+      allowEdit: true,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    }).then(imageData => {
+      this.selectedImage = `data:image/jpeg;base64,${imageData}`
+    })
+  }
+
+  recognizeImage() {
+    Tesseract.recognize(this.selectedImage);
+    // /.progress(message => {
+    //   if (this.message.status === 'recognizing text') {
+    //     this.progress.set(message.progress);
+    //   }
+    // })
+    // .then(result => {
+    //   this.imageText = result.text;
+    // }).catch (error => {
+    //   console.log('error!!!!');
+    // });
   }
 
 }
